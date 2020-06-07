525eace95eaf88976129fb9eafa05ef421f6ea54
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-06-07 11:46:14 -0300

    feat: variables

---
+++ variables.py
@@ -0,0 +1,32 @@
+# one line comment
+
+'''
+multi line coments
+'''
+
+#x = 1  # int
+#x = 2.5 # float
+#name = 'Ilgsson' # str
+#is_cool = True # bool
+
+# Multiple assignment
+
+x, y, name, is_cool = (1, 2.5, 'Ilgsson', True)
+
+print(x, y, name, is_cool)
+
+# Basic math
+
+print(x + y)
+
+# Type
+
+print(type(name))
+
+# Changing type
+
+x = str(x)
+
+print(x)
+
+print(type(x))
